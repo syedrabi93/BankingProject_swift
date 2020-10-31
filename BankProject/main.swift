748a9fd4c52f63b311e5a55463e17e68824efd14
syedrabi <syedrabi@amazon.com>
2020-10-31 17:20:10 +0530

    first commit

---
+++ BankProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BankProject
+//
+//  Created by Rabiyama, Syed on 10/31/20.
+//  Copyright © 2020 Rabiyama, Syed. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
